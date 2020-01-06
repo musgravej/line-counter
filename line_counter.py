949ce8f2a533ecfec7f6d4671add5455b54c389d
Jim Musgrave <musgravej@users.noreply.github.com>
2020-01-06 16:40:24 -0600

    updates
    Changes made to improve report formatting

--- line_counter.py
+++ line_counter.py
@@ -1,25 +1,28 @@
 import os
 
 
-def write_report(line_counts, longest_filename):
+def write_report(line_counts, longest_filename, most_lines):
+    fld2w = max(len("Count"), len("{:,}".format(most_lines)))
     with open("line_count_report.txt", 'w', newline="") as r:
         r.writelines("{fld1:<{fld1w}}{fld2:>{fld2w}}\n".format(fld1="File Name", fld2="Count", 
                                                                fld1w=longest_filename + 2,
-                                                               fld2w=10,))
+                                                               fld2w=fld2w,))
+        
         r.writelines("{fld1:<{fld1w}}{fld2:>{fld2w}}\n".format(fld1="-" * longest_filename, 
-                                                               fld2="-" * 10, 
+                                                               fld2="-" * fld2w, 
                                                                fld1w=longest_filename,
-                                                               fld2w=12,))
+                                                               fld2w=(fld2w + 2),))
         for file, count in line_counts.items():
-            print(file, count)
             if isinstance(count, (int,)):
+                print(file, "|", "{:,}".format(count))
                 r.writelines("{fld1:<{fld1w}}{fld2:>{fld2w},}\n".format(fld1=file, fld2=count, 
-                                                                   fld1w=longest_filename + 2,
-                                                                   fld2w=10,))
+                                                                        fld1w=longest_filename + 2,
+                                                                        fld2w=fld2w,))
             else:
+                print(file, "|", "{:}".format(count))
                 r.writelines("{fld1:<{fld1w}}{fld2:>{fld2w}}\n".format(fld1=file, fld2=count, 
-                                                                   fld1w=longest_filename + 2,
-                                                                   fld2w=10,))
+                                                                       fld1w=longest_filename + 2,
+                                                                       fld2w=fld2w,))
 
 
 def file_len(fname):
@@ -28,8 +31,10 @@ def file_len(fname):
             pass
     return i + 1
 
+
 def main():
     longest_filename = 0
+    most_lines = 0
     line_counts = dict()
 
     files = [f for f in os.listdir(os.path.curdir) 
@@ -39,13 +44,14 @@ def main():
         for f in files:
             try:
                 lines = file_len(f)
+                most_lines = max(most_lines, lines)
             except Exception as e:
                 lines = "n/a"
 
             line_counts[f] = lines
             longest_filename = max(longest_filename, len(f))
 
-        write_report(line_counts, longest_filename)
+        write_report(line_counts, longest_filename, most_lines)
 
 
 if __name__ == '__main__':
